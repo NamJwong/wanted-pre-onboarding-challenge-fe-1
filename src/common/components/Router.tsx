24a1ee395de02a652bc4193bf1866c78cded5290
jynam <njy1007@gmail.com>
2022-08-06 02:51:26 +0900

    feat: 홈 페이지 추가 및 라우팅 수정

--- src/common/components/Router.tsx
+++ src/common/components/Router.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Todo from 'todo';
 import Auth from 'auth';
+import Home from 'home';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Todo />} />
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
