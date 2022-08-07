f59dd519b0c4b0ce76e73a9d0dd3bce545598cbf
jynam <njy1007@gmail.com>
2022-08-08 04:14:03 +0900

    feat: 완료 버튼 활성화 조건 걸기

--- src/auth/AuthForm.tsx
+++ src/auth/AuthForm.tsx
@@ -1,5 +1,6 @@
 import Form from 'common/components/Form';
 import FormItem from 'common/components/FormItem';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 interface AuthFormProps {
@@ -19,18 +20,37 @@ interface AuthFormProps {
 
 export default function AuthForm(props: AuthFormProps) {
   const { email, password } = props;
+  const [disabledButton, setDisabledButton] = useState(true);
+
+  useEffect(() => {
+    if (email.value && password.value) {
+      if (email.errorMessage) {
+        if (email.isValid && password.isValid) setDisabledButton(false);
+        else setDisabledButton(true);
+      } else setDisabledButton(false);
+    } else {
+      setDisabledButton(true);
+    }
+  }, [email, password]);
+
   return (
     <Form>
       <FormItem label="이메일" value={email.value} onChange={email.onChange} width="150px" />
-      {email.isValid || <StErrorMessage>{email.errorMessage}</StErrorMessage>}
+      {email.errorMessage && !email.isValid && (
+        <StErrorMessage>{email.errorMessage}</StErrorMessage>
+      )}
       <FormItem
         label="비밀번호"
         value={password.value}
         onChange={password.onChange}
         width="150px"
       />
-      {password.isValid || <StErrorMessage>{password.errorMessage}</StErrorMessage>}
-      <StButton>완료</StButton>
+      {password.errorMessage && !password.isValid && (
+        <StErrorMessage>{password.errorMessage}</StErrorMessage>
+      )}
+      <StButton disabled={disabledButton} onClick={() => console.log('click')}>
+        완료
+      </StButton>
     </Form>
   );
 }
