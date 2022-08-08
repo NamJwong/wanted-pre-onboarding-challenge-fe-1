faca83a10063aa00cc14fb2a7eb404ad4feae1a4
jynam <njy1007@gmail.com>
2022-08-09 03:17:44 +0900

    design: 시작하기 버튼을 로그인, 회원가입 버튼으로 변경

--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
+
 export default function Landing() {
   return (
-    <div>
+    <StLanding>
       <h1>
         안녕하세요! <br />
         <a href="https://github.com/NamJwong">NamJwong</a>이 만든 투두리스트 앱입니다.
       </h1>
-      <button>시작하기</button>
-    </div>
+      <button>로그인</button>
+      <button>회원가입</button>
+    </StLanding>
   );
 }
+
+const StLanding = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  & > button {
+    width: 100px;
+  }
+  & > h1 {
+    text-align: center;
+    line-height: 120%;
+  }
+`;
